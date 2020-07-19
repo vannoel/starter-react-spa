8c2b0600d25ad18498c668df4db42cc6754ab01d
vannoel <vannoel0628@gmail.com>
2020-07-19 21:29:16 +0800

    Update axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,17 +1,10 @@
 import axios from 'axios';
 
-import { store } from '@src/store.js';
-
 const instance = axios.create({
   baseURL : `${process.env.API_URL}`
 });
 
 instance.interceptors.request.use(function (config) {
-  let xAccessToken = store.getState().user.xAccessToken || '';
-  if (xAccessToken)
-  {
-    config.headers['X-Access-Token'] = `${xAccessToken}`;
-  }
   return Promise.resolve(config);
 }, function (error) {
   return Promise.reject(error);
